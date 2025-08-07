6724a49681d479d8c3bbec8887f243780961c974
David_MacOS <david.martin@univ-rennes2.fr>
2025-08-07 19:40:10 +0200

    config update

--- publishconf.py
+++ publishconf.py
@@ -8,8 +8,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://djrmartin.github.io/DavidMartin.github.io/"
-RELATIVE_URLS = True
+SITEURL = 'https://djrmartin.github.io/DavidMartin.github.io'
+RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
